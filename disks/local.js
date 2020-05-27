8f213f2099d64f2683822012ef8f1153226e3f53
Josh Boudreau <joshuaboud@hotmail.com>
2020-05-27 11:04:32 -0300

    rm set env variables

--- disks/local.js
+++ disks/local.js
@@ -28,7 +28,6 @@ function grab_info() {
     err_box.style.display = "none";
     document.getElementById("warning-box").style.display = "none";
     var proc = cockpit.spawn(["/usr/bin/pkexec",
-    "/usr/bin/env","ALIAS_DEVICE_PATH=$ALIAS_DEVICE_PATH","ALIAS_CONFIG_PATH=$ALIAS_CONFIG_PATH",
     "/usr/bin/lsdev","--json"], {err: "out"});
     proc.always(function() {
         document.getElementById("loading").style.display = "none";
